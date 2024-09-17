1d39601f107c728b34883581dd05951c404225c3
AYUSH GUPTA <98117063+ayush-1123@users.noreply.github.com>
2024-09-17 22:44:19 +0000

    numsMaths pending

---
+++ 01_Basics/numsMaths.js
@@ -0,0 +1,46 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toString().charAt(0));
+//console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+//console.log(otherNumber.toPrecision(2));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
+
+Number
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE);
+
+// +++++++++++++++++++++  Maths  +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(6.8));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
