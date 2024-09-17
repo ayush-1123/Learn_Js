5a241db41b1e9b8603c51bb0f3824ff81c433b57
AYUSH GUPTA <98117063+ayush-1123@users.noreply.github.com>
2024-09-17 12:50:28 +0000

    todays commit

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,30 @@
+// Operations
+str1 = "hello"
+str2 = " world"
+str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let x = 3
+// const y = x++ // postfix operation => x, y = 4, 3
+// console.log([x , y]);
+
+let x = 3
+const y = ++x // postfix operation => x, y = 4, 4
+//console.log([x , y]);
+
+
+
+
+
+
+
+
+
---
+++ 01_Basics/datatypeConversion.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueNumber = Number(score)
+//console.log(typeof valueNumber);
+//console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN (not an number)
+// true => 1 false => 0
+
+let isLoggedin = 1
+let isBoolean = Boolean(isLoggedin)
+//console.log(isBoolean);
+
+// "" => false
+// "ayush" => true
+
+let randomNumber = 33
+let stringNumber = String(randomNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
