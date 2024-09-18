c2e59fecf45efb167cfe42787b2387627b50cac6
AYUSH GUPTA <98117063+ayush-1123@users.noreply.github.com>
2024-09-18 10:39:28 +0000

    dates incomplete

---
+++ 01_Basics/dates.js
@@ -0,0 +1,18 @@
+const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+const myCreatedDate = new Date(2024, 9, 8) // Date(Year, Month, Date) => months starts from 0-11
+console.log(myCreatedDate.toLocaleString());
+
+
+
+
+
+
+
