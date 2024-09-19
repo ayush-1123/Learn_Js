206ba1854351d6be819aba0fe49844cd92d9af89
AYUSH GUPTA <98117063+ayush-1123@users.noreply.github.com>
2024-09-19 09:43:01 +0000

    dates completed

--- 01_Basics/dates.js
+++ 01_Basics/dates.js
@@ -1,3 +1,4 @@
+// all dates are calculated with the refernece Januaury 1,1970
 const myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
@@ -5,10 +6,25 @@ const myDate = new Date()
 // console.log(myDate.toLocaleDateString());
 // console.log(myDate.toISOString());
 // console.log(myDate.toJSON());
-// console.log(typeof myDate);
+// console.log(typeof myDate);  => Object
 
-const myCreatedDate = new Date(2024, 9, 8) // Date(Year, Month, Date) => months starts from 0-11
-console.log(myCreatedDate.toLocaleString());
+//const myCreatedDate = new Date(2024, 9, 8) // Date(Year, Month, Date) => months starts from 0-11
+//const myNewDate = new Date(2024, 9, 8, 5, 25, 52)
+const myNewDate = new Date("08-10-2024")
+//console.log(myNewDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp); // gives time in 'ms' from jan 1,1970
+// console.log(myNewDate.getTime()); //gives time from jan 1,1970 and 8-10-2024
+
+let getDate = new Date()
+console.log(getDate);
+console.log(getDate.getMonth() + 1);
+
+
+
+
+
 
 
 
